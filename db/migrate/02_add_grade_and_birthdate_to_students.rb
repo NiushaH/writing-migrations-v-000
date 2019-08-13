0c4d2ad890988bd020b0fd30a3b280e051a9fe99
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-13 19:01:08 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[4.2]
+# class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[4.2]
 
-  def change
-    add_column :students, :grade, :integer
-    add_column :students, :birthdate, :integer
-  end
-end
+#   def change
+#     add_column :students, :grade, :integer
+#     add_column :students, :birthdate, :integer
+#   end
+# end
